f495c3e01909b1bcbc8ecf41f687add1add03ae6
xezon <4720891+xezon@users.noreply.github.com>
2021-03-11 13:46:22 +0100

    Add ControlBarHDZH source files

---
+++ ControlBarHDZH/MAKE_Release.bat
@@ -0,0 +1,5 @@
+:: TODO: Auto .big archive and auto .7z archive all big contents
+
+set ReleaseDir=Release
+if not exist %ReleaseDir%\7z mkdir %ReleaseDir%\7z
+if not exist %ReleaseDir%\big mkdir %ReleaseDir%\big
