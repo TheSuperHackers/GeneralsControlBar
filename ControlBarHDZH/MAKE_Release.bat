0bfc93e6b8ed35a3c4494d29b742fdf2a1af9358
xezon <4720891+xezon@users.noreply.github.com>
2021-04-24 16:31:20 +0200

    Improve build scripts for Control Bar HD

--- ControlBarHDZH/MAKE_Release.bat
+++ ControlBarHDZH/MAKE_Release.bat
@@ -1,5 +1,65 @@
-:: TODO: Auto .big archive and auto .7z archive all big contents
+:: Generates Big File contents that can be put in .big files
 
+setlocal enableextensions enabledelayedexpansion
+
+set ArchiveName=ControlBarHDZH_v1.0
+
+:: Setup folders
+set ToolsDir=..\Tools
+set GameFilesDir=GameFilesEdited
+set GeneratedBigFilesUnpackedDir=.Generated\BigFilesUnpacked
+set GeneratedBigFilesDir=.Generated\BigFiles
+set ReleaseUnpackedDir=ReleaseUnpacked
 set ReleaseDir=Release
-if not exist %ReleaseDir%\7z mkdir %ReleaseDir%\7z
-if not exist %ReleaseDir%\big mkdir %ReleaseDir%\big
+set BigNameBegin=400_ControlBarHD
+set BigNameEnd=ZH
+
+:: Create folders
+if not exist %GeneratedBigFilesUnpackedDir% mkdir %GeneratedBigFilesUnpackedDir%
+if not exist %GeneratedBigFilesDir% mkdir %GeneratedBigFilesDir%
+if not exist %ReleaseDir% mkdir %ReleaseDir%
+
+
+:: Create Control Bar HD Base
+set BaseBigName=%BigNameBegin%Base%BigNameEnd%
+
+:: Free folders of big file contents
+del /s /f /q %GeneratedBigFilesUnpackedDir%\%BaseBigName%
+del /s /f /q %GeneratedBigFilesDir%\%BaseBigName%.big
+
+:: Copy .big contents
+set SrcArtTexturesDir=%GameFilesDir%\Art\Textures
+set DestArtTexturesDir=%GeneratedBigFilesUnpackedDir%\%BaseBigName%\Art\Textures
+if not exist %DestArtTexturesDir% mkdir %DestArtTexturesDir%
+xcopy /Y %SrcArtTexturesDir% %DestArtTexturesDir%
+
+:: Generate .big file(s)
+%ToolsDir%\GeneralsBigCreator\GeneralsBigCreator.exe -source %GeneratedBigFilesUnpackedDir%\%BaseBigName% -dest %GeneratedBigFilesDir%\%BaseBigName%.big
+
+
+for %%l in (Brazilian,Chinese,English,French,German,Italian,Korean,Polish,Spanish) do (
+	:: Create Control Bar HD Languages
+	set LanguageBigName=%BigNameBegin%%%l%BigNameEnd%
+	set GeneratedReleaseUnpackedDir=.Generated\ReleaseUnpacked%%l
+	
+	:: Free folders of big file contents
+	del /s /f /q %GeneratedBigFilesUnpackedDir%\!LanguageBigName!
+	del /s /f /q %GeneratedBigFilesDir%\!LanguageBigName!.big
+	
+	:: Copy .big contents
+	set SrcDataLangArtTexturesDir=%GameFilesDir%\Data\Language\Art\Texture
+	set DestDataLangArtTexturesDir=%GeneratedBigFilesUnpackedDir%\!LanguageBigName!\Data\%%l\Art\Textures
+	if not exist !DestDataLangArtTexturesDir! mkdir !DestDataLangArtTexturesDir!
+	xcopy /Y !SrcDataLangArtTexturesDir! !DestDataLangArtTexturesDir!
+
+	:: Generate .big file(s)
+	%ToolsDir%\GeneralsBigCreator\GeneralsBigCreator.exe -source %GeneratedBigFilesUnpackedDir%\!LanguageBigName! -dest %GeneratedBigFilesDir%\!LanguageBigName!.big
+	
+	:: Generate Release file(s)
+	xcopy /Y %GeneratedBigFilesDir%\%BaseBigName%.big !GeneratedReleaseUnpackedDir!\*
+	xcopy /Y %GeneratedBigFilesDir%\!LanguageBigName!.big !GeneratedReleaseUnpackedDir!\*
+	xcopy /Y %ReleaseUnpackedDir%\* !GeneratedReleaseUnpackedDir!\*
+	
+	:: Generate Archive(s)
+	tar.exe -a -c -C !GeneratedReleaseUnpackedDir! -f %ReleaseDir%\%ArchiveName%_%%l.zip *.*
+)
--- ControlBarHDZH/ReleaseUnpacked/ReadMe.txt
+++ ControlBarHDZH/ReleaseUnpacked/ReadMe.txt
@@ -2,25 +2,21 @@ Control Bar HD Remastered Pack for COMMAND AND CONQUER GENERALS: ZERO HOUR
 
 Author(s): EA Games, xezon
 
+Original download from: http://gentool.net/download/controlbar
+
 Code & Assets: https://github.com/xezon/GeneralsControlBar
 
------------------------------------------------------------------------------------------------
---- Download ----------------------------------------------------------------------------------
-
-You can either download the .big or .7z file(s). 7z can be unpacked with the program 7-zip.
 
 -----------------------------------------------------------------------------------------------
 --- Install -----------------------------------------------------------------------------------
 
-1. Copy the "400_ControlBarHDBaseZH.big" file into the game install directory, for example:
+1. Copy all files into the game install directory, for example:
    > C:\Program Files (x86)\EA Games\Command & Conquer Generals Zero Hour
-
-2. Copy the "400_ControlBarHD%YOUR_LANGUAGE%ZH.big" file into the game install directory too.
-   > %YOUR_LANGUAGE% corresponds to the game install language
    
    > On English install it would then look like so:
    > ...\EA Games\Command & Conquer Generals Zero Hour\400_ControlBarHDBaseZH.big
    > ...\EA Games\Command & Conquer Generals Zero Hour\400_ControlBarHDEnglishZH.big
+ 
 
 -----------------------------------------------------------------------------------------------
 --- REMARKS -----------------------------------------------------------------------------------
@@ -28,3 +24,10 @@ You can either download the .big or .7z file(s). 7z can be unpacked with the pro
 This pack improves texture quality by 150-200% on Control Bar only.
 Uses textures 4 times larger than usual. Use with high resolution only.
 If you play with 800x600 then this pack has no effect. Game will use more process memory.
+
+
+-----------------------------------------------------------------------------------------------
+--- CHANGELOG ---------------------------------------------------------------------------------
+
+v1.0:
+- Initial release
