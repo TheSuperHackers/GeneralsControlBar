c6ac4d9d266dd8850ae0723c6c23689298882cb5
xezon <4720891+xezon@users.noreply.github.com>
2021-06-01 19:35:06 +0200

    Use ControlBarProScrollListBox* HeaderTemplate in all SCROLLISTBOX window elements

--- ControlBarProZH/MAKE_Release.bat
+++ ControlBarProZH/MAKE_Release.bat
@@ -11,9 +11,11 @@ call MAKE_ControlBarProResXZH.bat 2160
 set ArchiveName=ControlBarProZH_v1.2
 
 :: Generate Archive(s)
-tar.exe -a -c -C %GeneratedReleaseUnpackedDir% -f %ReleaseDir%\%ArchiveName%.zip *.*
 tar.exe -a -c -C %GeneratedReleaseUnpacked720Dir% -f %ReleaseDir%\%ArchiveName%_1280x720.zip *.*
 tar.exe -a -c -C %GeneratedReleaseUnpacked900Dir% -f %ReleaseDir%\%ArchiveName%_1600x900.zip *.*
 tar.exe -a -c -C %GeneratedReleaseUnpacked1080Dir% -f %ReleaseDir%\%ArchiveName%_1920x1080.zip *.*
 tar.exe -a -c -C %GeneratedReleaseUnpacked1440Dir% -f %ReleaseDir%\%ArchiveName%_2560x1440.zip *.*
 tar.exe -a -c -C %GeneratedReleaseUnpacked2160Dir% -f %ReleaseDir%\%ArchiveName%_3840x2160.zip *.*
+
+:: LEGACY Release without ResX big file no longer works. All SCROLLLISTBOX use new HeaderTemplate font.
+::tar.exe -a -c -C %GeneratedReleaseUnpackedDir% -f %ReleaseDir%\%ArchiveName%.zip *.*
