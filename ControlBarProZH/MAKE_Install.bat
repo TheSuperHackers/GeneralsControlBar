208bbb3dcfcb68bb898b9c2c994368f0864b7b94
xezon <4720891+xezon@users.noreply.github.com>
2021-09-19 11:33:03 +0200

    Add admin privilege request to ControlBarPro build scripts

--- ControlBarProZH/MAKE_Install.bat
+++ ControlBarProZH/MAKE_Install.bat
@@ -1,8 +1,36 @@
-call MAKE_ControlBarProHideIpZH.bat
-call MAKE_ControlBarProHideMailZH.bat
-call MAKE_ControlBarProZH.bat
+@echo off
+:: BatchGotAdmin
+:-------------------------------------
+::  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+:: --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+echo on
+
+set ThisDir0=%~dp0
+call %ThisDir0%MAKE_ControlBarProHideIpZH.bat
+call %ThisDir0%MAKE_ControlBarProHideMailZH.bat
+call %ThisDir0%MAKE_ControlBarProZH.bat
 
 set GameRootDir="C:\Program Files (x86)\EA Games\Command & Conquer The First Decade\Command & Conquer(tm) Generals Zero Hour"
 
 ::Copy release files to game
-xcopy /Y /S %GeneratedReleaseUnpackedDir% %GameRootDir%
+xcopy /y /s %GeneratedReleaseUnpackedDir% %GameRootDir%
