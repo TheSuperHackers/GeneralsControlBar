c842b6f19780da8f4a4b432012b6c0cd344960c2
xezon <4720891+xezon@users.noreply.github.com>
2021-09-26 13:38:41 +0200

    Add CrunchTextures.bat script

---
+++ ControlBarProZH/Scripts/CrunchTextures.bat
@@ -0,0 +1,37 @@
+:: Argument 1 : Folder to textures
+
+set TextureDir=%1
+
+if "%TextureDir%" == "" (
+  echo "First argument must be directory to texture(s)"
+) else (
+  set TextureInfoFile=%TextureDir%\CrunchInfo.txt
+  :: Compress PSD to DDS and delete intermediate source copies
+  for /r %TextureDir% %%f in (*.psd) do (
+    %ToolsDir%\crunch\crunch_x64.exe -file %%f -info >%TextureInfoFile%
+    findstr /c:"A8L8" /c:"A8R8G8B8" %TextureInfoFile% >nul
+    if not ERRORLEVEL 1 (
+      :: Texture has alpha channel
+      %ToolsDir%\crunch\crunch_x64.exe ^
+        -file %%f ^
+        -fileformat dds ^
+        -outsamedir ^
+        -dxt5 ^
+        -quality 255 ^
+        -mipmode none ^
+        -lzmastats
+    ) else (
+      :: Texture does not have alpha channel
+      %ToolsDir%\crunch\crunch_x64.exe ^
+        -file %%f ^
+        -fileformat dds ^
+        -outsamedir ^
+        -dxt1 ^
+        -quality 255 ^
+        -mipmode none ^
+        -lzmastats
+    )
+    del /f /q %%f
+  )
+  del /f /q %TextureInfoFile%
+)
--- ControlBarProZH/Scripts/MAKE_ControlBarProZH.bat
+++ ControlBarProZH/Scripts/MAKE_ControlBarProZH.bat
@@ -19,17 +19,8 @@ xcopy /y /s %GameFilesDir%\*.wnd %GeneratedBigFilesUnpackedDir%\%BigName%\
 xcopy /y /s %GameFilesDir%\*.dat %GeneratedBigFilesUnpackedDir%\%BigName%\
 xcopy /y /s %GameFilesDir%\*.txt %GeneratedBigFilesUnpackedDir%\%BigName%\
 
-:: Compress TGA and PSD to DDS and delete intermediate source copies
-for /r %GeneratedBigFilesUnpackedDir%\%BigName%\ %%f in (*.tga, *.psd) do (
-    %ToolsDir%\crunch\crunch_x64.exe ^
-	-file %%f ^
-	-fileformat dds ^
-	-outsamedir -dxt5 ^
-	-quality 255 ^
-	-mipmode none ^
-	-lzmastats
-	del /f /q %%f
-)
+:: Compress Textures to DDS and delete intermediate source copies
+call %ThisDir1%\CrunchTextures.bat %GeneratedBigFilesUnpackedDir%\%BigName%\
 
 :: Generate .big file(s)
 %ToolsDir%\GeneralsBigCreator\GeneralsBigCreator.exe -source %GeneratedBigFilesUnpackedDir%\%BigName% -dest %GeneratedBigFilesDir%\%BigName%.big
