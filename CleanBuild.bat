6bf68a03966393f858afb9e01d2c95af51aa2a7b
xezon <4720891+xezon@users.noreply.github.com>
2022-04-23 18:55:04 +0200

    Add Generals Mod Builder for Control Bar HD and build DDS textures

---
+++ CleanBuild.bat
@@ -0,0 +1,8 @@
+set ThisDir=%~dp0.
+
+rd /s /q "%ThisDir%\ControlBarHDZH\.Build"
+rd /s /q "%ThisDir%\ControlBarHDZH\.Release"
+rd /s /q "%ThisDir%\ControlBarObsZH\.Build"
+rd /s /q "%ThisDir%\ControlBarObsZH\.Release"
+rd /s /q "%ThisDir%\ControlBarProZH\.Build"
+rd /s /q "%ThisDir%\ControlBarProZH\.Release"
