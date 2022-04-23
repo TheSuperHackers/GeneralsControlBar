c5673ef1da9149f2de7ac529badf1eb14e404c5d
xezon <4720891+xezon@users.noreply.github.com>
2022-04-23 09:28:26 +0200

    Remove all legacy build scripts

--- ControlBarObsZH/MAKE_Install.bat
+++
@@ -1,6 +0,0 @@
-call MAKE_Release.bat
-
-set GameRootDir="C:\Program Files (x86)\EA Games\Command & Conquer The First Decade\Command & Conquer(tm) Generals Zero Hour"
-
-::Copy release files to game
-xcopy /Y /S %ReleaseUnpackedDir% %GameRootDir%
