39b7712d5815ddb56cbb49233f6812b9a36ea228
xezon <4720891+xezon@users.noreply.github.com>
2022-04-23 20:47:45 +0200

    Add Batch scripts to toggle Control Bar Obs Addon files

---
+++ ControlBarObsZH/ReleaseUnpacked/ControlBarObs_ON_OFF.bat
@@ -0,0 +1,69 @@
+@echo off
+:-------------------------------------
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else (
+    goto gotAdmin
+)
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+set ThisDir=%~dp0.
+set FileList1=
+set FileList1=%FileList1% 200_ControlBarObsBaseZH.big
+set FileList1=%FileList1% 200_ControlBarObsEnglishZH.big
+set FileList2=
+set FileList2=%FileList2% 250_HideIP_WindowZH.big
+set FileList2=%FileList2% 251_HideMail_WindowZH.big
+set SetOn=0
+set RenSuffix=off
+
+for %%f in (%FileList1%) do (
+    if exist "%ThisDir%\%%f.%RenSuffix%" (
+        set SetOn=1
+        goto next
+    )
+)
+
+:next
+
+for %%f in (%FileList1%) do (
+    if %SetOn% EQU 0 (
+        if exist "%ThisDir%\%%f" (
+            echo Rename "%ThisDir%\%%f"
+            echo     to "%ThisDir%\%%f.%RenSuffix%"
+            rename "%ThisDir%\%%f" "%%f.%RenSuffix%"
+        )
+    ) else (
+        if exist "%ThisDir%\%%f.%RenSuffix%" (
+            echo Rename "%ThisDir%\%%f.%RenSuffix%""
+            echo     to "%ThisDir%\%%f"
+            rename "%ThisDir%\%%f.%RenSuffix%" "%%f"
+        )
+    )
+)
+
+if %SetOn% EQU 0 (
+    for %%f in (%FileList2%) do (
+        if exist "%ThisDir%\%%f" (
+            echo Rename "%ThisDir%\%%f"
+            echo     to "%ThisDir%\%%f.%RenSuffix%"
+            rename "%ThisDir%\%%f" "%%f.%RenSuffix%"
+        )
+    )
+)
