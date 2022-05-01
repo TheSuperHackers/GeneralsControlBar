98efcfad9a88ca800f7e4e45281eb81d676193c6
xezon <4720891+xezon@users.noreply.github.com>
2022-05-01 11:07:26 +0200

    Upgrade to ModBuilder 1.5

--- CleanBuild.bat
+++ CleanBuild.bat
@@ -2,8 +2,11 @@ set ThisDir=%~dp0.
 
 rd /s /q "%ThisDir%\ControlBarHDZH\.Build"
 rd /s /q "%ThisDir%\ControlBarHDZH\.Release"
+rd /s /q "%ThisDir%\ControlBarHDZH\Scripts\Windows\.modbuilder"
 rd /s /q "%ThisDir%\ControlBarObsZH\.Build"
 rd /s /q "%ThisDir%\ControlBarObsZH\.Release"
+rd /s /q "%ThisDir%\ControlBarObsZH\Scripts\Windows\.modbuilder"
 rd /s /q "%ThisDir%\ControlBarProZH\.Build"
 rd /s /q "%ThisDir%\ControlBarProZH\.Release"
 rd /s /q "%ThisDir%\ControlBarProZH\Scripts\Python\__pycache__"
+rd /s /q "%ThisDir%\ControlBarProZH\Scripts\Windows\.modbuilder"
